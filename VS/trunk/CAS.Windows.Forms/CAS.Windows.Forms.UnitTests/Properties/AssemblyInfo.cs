9a9da12b741d1028b099a3fdb2ed0a46600b821a
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2016-02-16 21:23:19 +0000

    Architecture improvements - working on the project CAS.Windows.Forms - tested.
    Issue #: 4772

--- VS/trunk/CAS.Windows.Forms/CAS.Windows.Forms.UnitTests/Properties/AssemblyInfo.cs
+++ VS/trunk/CAS.Windows.Forms/CAS.Windows.Forms.UnitTests/Properties/AssemblyInfo.cs
@@ -1,26 +1,24 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using CAS;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle( "BK.ControlLibrary.UT" )]
-[assembly: AssemblyDescription( "Tests for CAS.ControlLibrary" )]
-[assembly: AssemblyConfiguration( "" )]
-[assembly: AssemblyCompany( "CAS" )]
-[assembly: AssemblyProduct( "BK.ControlLibrary.UT" )]
-[assembly: AssemblyCopyright( "CAS (c) 2014" )]
-[assembly: AssemblyTrademark( "" )]
-[assembly: AssemblyCulture( "" )]
+[assembly: AssemblyTitle("BK.ControlLibrary.UT")]
+[assembly: AssemblyDescription("Tests for CAS.ControlLibrary")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CAS")]
+[assembly: AssemblyProduct("BK.ControlLibrary.UT")]
+[assembly: AssemblyCopyright("CAS (c) 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM componenets.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible( false )]
+[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid( "ab1adb44-affb-4735-bdf1-8d37533eed8b" )]
-[assembly: AssemblyVersion( "5.0.0.*" )]
-[assembly: AssemblyFileVersion( "5.0.0" )]
+[assembly: Guid("ab1adb44-affb-4735-bdf1-8d37533eed8b")]
+[assembly: AssemblyVersion("5.0.0.*")]
+[assembly: AssemblyFileVersion("5.0.0")]
--- VS/trunk/CAS.Windows.Forms/CAS.Windows.Forms/About.cs
+++ VS/trunk/CAS.Windows.Forms/CAS.Windows.Forms/About.cs
@@ -24,6 +24,7 @@ namespace CAS.Lib.ControlLibrary
 {
   public partial class About: UserControl
   {
+
     #region public
     /// <summary>
     /// Component shows CAS about information
