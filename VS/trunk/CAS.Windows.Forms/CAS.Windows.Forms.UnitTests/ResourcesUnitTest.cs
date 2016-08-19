ba955765dd429a918aa616faebcd7030ac2b23d3
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2016-08-19 21:03:44 +0000

    CommServer.DA.Server - remove direct dependencies - trunk  CAS.Windows.Forms added Resources Unit Test, changed Release version 5.0.6-Bravo. Issue #: 4859

---
+++ VS/trunk/CAS.Windows.Forms/CAS.Windows.Forms.UnitTests/ResourcesUnitTest.cs
@@ -0,0 +1,21 @@
+﻿
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Drawing;
+
+namespace CAS.Windows.FormsUnitTests
+{
+  [TestClass]
+  public class ResourcesUnitTest
+  {
+    [TestMethod]
+    public void ImagesTestMethod()
+    {
+      Bitmap _startBitmap = Forms.Properties.Resources.start;
+      Assert.IsNotNull(_startBitmap);
+      _startBitmap = Forms.Properties.Resources.stop;
+      Assert.IsNotNull(_startBitmap);
+      _startBitmap = Forms.Properties.Resources.restart;
+      Assert.IsNotNull(_startBitmap);
+    }
+  }
+}
