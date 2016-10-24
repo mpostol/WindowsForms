065268cea4bcb508f3b068255df9c11986ee0b23
mpostol <mpostol@8a3894fd-0c01-b944-991b-6a3da02cae8a>
2016-10-24 11:53:30 +0000

    CAS new release - working on CAS.Windows.Forms - created tag creation script - ready to release 6.0.1 - Unit tested. Issue #: 4878

---
+++ VS/trunk/CAS.Windows.Forms/CAS.Windows.Forms.Scripts/CreateTag.cmd
@@ -0,0 +1,13 @@
+
+rem//  $LastChangedDate:  $
+rem//  $Rev:  $
+rem//  $LastChangedBy:  $
+rem//  $URL:  $
+rem//  $Id:  $
+
+
+svn mkdir svn://svnserver.hq.cas.com.pl/VS/tags/CAS.Windows.Forms.rel_6_0_1  -m "created new svn://svnserver.hq.cas.com.pl/VS/tags/CAS.Windows.Forms.rel_6_0_1"
+svn copy svn://svnserver.hq.cas.com.pl/VS/trunk/CAS.Windows.Forms/ svn://svnserver.hq.cas.com.pl/VS/tags/CAS.Windows.Forms.rel_6_0_1 -m "created copy svn://svnserver.hq.cas.com.pl/VS/tags/CAS.Windows.Forms.rel_6_0_1"
+
+pause ....
+
---
+++ VS/trunk/CAS.Windows.Forms/CAS.Windows.Forms.Scripts/Properties/AssemblyInfo.cs
@@ -0,0 +1,31 @@
+﻿//_______________________________________________________________
+//  Title   :  Assembly info for: CAS.Windows.Forms.Scripts
+//  System  : Microsoft VisualStudio 2015 / C#
+//  $LastChangedDate:  $
+//  $Rev:  $
+//  $LastChangedBy: $
+//  $URL:  $
+//  $Id:  $
+//
+//  Copyright (c) 2000-2016 CAS LODZ POLAND
+//  +48 (42) 686 25 47
+//  techsupp@cas.eu
+//  www.cas.eu
+//_______________________________________________________________
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CAS.Windows.Forms.Scripts")]
+[assembly: AssemblyDescription("Scripts Prototyping Workspace for CAS.Windows.Forms.Scripts")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CAS")]
+[assembly: AssemblyProduct("CAS CommServer Family")]
+[assembly: AssemblyCopyright("Copyright (c) 2000-2016 CAS LODZ POLAND")]
+[assembly: AssemblyTrademark("CommServer")]
+[assembly: AssemblyCulture("")]
+[assembly: Guid("1978B4D2-ACDF-4164-9677-BBEC3B858A45")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("6.0.1.*")]
+[assembly: AssemblyFileVersion("6.0.1")]
+
